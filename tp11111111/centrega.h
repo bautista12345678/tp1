e67eba7e8677f5f3c18f9d70729c4fcd9c9ccb59
bautista12345678 <bautivaldes2014@gmail.com>
2022-09-19 18:42:47 -0300

    aa

---
+++ tp11111111/centrega.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<string>
+#include <ctime>
+#include<iostream>
+#include"EnumEstadoDeProyecto.h"
+#include"EnumEtapaDeProyecto.h"
+#include"Cjefe.h"
+using namespace std;
+class Centrega
+{
+private:
+	EtapaDelProyecto NumeroDeEtapa;
+	string IDdelProyecto;
+	tm FechaDeEntrega;
+	bool Aceptado;
+	int CantidadDeEntregas;
+public:
+	Centrega();
+	~Centrega();
+	void actualizar(Cjefe* jefe);
+	void establecerFechaDeEntrega(int dia, int mes, int anio);
+
+
+
+
+};
